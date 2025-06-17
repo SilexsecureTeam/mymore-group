7f2ea545855f51bc882891d8ed7eb8d8a995a540
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-06-17 12:58:18 +0100

    Update aboutBody.jsx

--- src/components/aboutBody.jsx
+++ src/components/aboutBody.jsx
@@ -152,7 +152,7 @@ export default function AboutBody() {
               {/* Name Below Description */}
               <p className="pt-6 text-[#780000] font-semibold text-xl">
                 The CEO<br/>
-                Ahmed Akande]<br/> Msc. Air Transport Mgt
+                Ahmed Akande<br/> Msc. Air Transport Mgt
               </p>
             </div>
           </div>
